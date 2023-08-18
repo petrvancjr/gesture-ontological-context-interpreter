65df27b0e82d0e3229d3922386d1e049210b26eb
Petr Vanc <petrvancjr@gmail.com>
2023-08-18 12:11:58 +0200

    add_triplet fun

--- add_to_ontology.py
+++ add_to_ontology.py
@@ -6,29 +6,51 @@ from knowrob.srv import TellQuery, TellQueryRequest
 from knowrob.msg import TripleStatement
 import time
 
-def add(q):
-    rospy.wait_for_service('/rosprolog/tell')
-    try:
-        add_proxy = rospy.ServiceProxy('/rosprolog/tell', TellQuery)
-        resp1 = add_proxy(q)
-        return resp1
-    except rospy.ServiceException as e:
-        print("Service call failed: %s"%e)
-
-def test():
+def add_triplet(s, p, o):
     q = TellQueryRequest()
-
     t = TripleStatement()
-    t.subject =  "http://www.ease-crc.org/ont/ciirc-gestures#PandaRobot"
-    t.property = "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#hasComponent"
-    t.object =   "http://www.ease-crc.org/ont/ciirc-gestures#PandaGripper"
+    t.subject = s
+    t.property = p
+    t.object = o
     t.startTime = time.time()
     t.endTime = time.time()
 
     q.statements = [t]
     q.reasoner = 'mongolog1'
     print(f"Requesting {q}")
-    print(f"Result: {add(q)}")
+
+    rospy.wait_for_service('/rosprolog/tell')
+    try:
+        add_proxy = rospy.ServiceProxy('/rosprolog/tell', TellQuery)
+        resp1 = add_proxy(q)
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
+        resp1 = False
+    print(f"Result: {resp1}")
+
+
+def test():
+    add_triplet("http://www.ease-crc.org/ont/ciirc-gestures#PandaRobot", \
+                "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#hasComponent", \
+                "http://www.ease-crc.org/ont/ciirc-gestures#PandaGripper")
+
+    add_triplet("http://www.ease-crc.org/ont/ciirc-gestures#Mug", \
+                "http://www.ease-crc.org/ont/SOMA.owl#isOntopOf", \
+                "http://www.ease-crc.org/ont/ciirc-gestures#Table")
+    
+    add_triplet("http://www.ease-crc.org/ont/ciirc-gestures#MustardBottle", \
+                "http://www.ease-crc.org/ont/SOMA.owl#isOntopOf", \
+                "http://www.ease-crc.org/ont/ciirc-gestures#Mug")
+    
+    add_triplet("http://www.ease-crc.org/ont/ciirc-gestures#Mug", \
+                "http://www.ease-crc.org/ont/SOMA.owl#hasColor", \
+                "http://www.ease-crc.org/ont/ciirc-gestures#Blue")
+
+    add_triplet("http://www.ease-crc.org/ont/ciirc-gestures#Table", \
+                "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#hasQuality", \
+                "http://www.ease-crc.org/ont/ciirc-gestures#LargeSize")
+
+
 
 if __name__ == "__main__":
     test()
